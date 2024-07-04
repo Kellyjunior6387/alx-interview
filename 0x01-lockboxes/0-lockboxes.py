04626d0a0ea90844989ee398b3ac7b538b2c68de
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-04 21:38:23 +0300

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,34 +1,25 @@
-#!/usr/bin/python3
-"""Find if all boxes can be unlocked"""
 from collections import deque
-
-"""def canUnlockAll(boxes):
-    visited_boxes = []
-    keys = deque([])
-    for key in boxes[0]:
-        visited_boxes.append(0)
-        keys.append(key)
-    while len(keys) != 0:
-        for i in boxes[keys[0]]:
-            if i not in visited_boxes and i not in keys:
-                keys.append(i)
-            visited_boxes.append(keys.popleft())
-            deque(sorted(keys))
-    return len(boxes) == len(visited_boxes)
-"""
+"""Function to check if all boxes can be opened"""
 
 
 def canUnlockAll(boxes):
-    visited = [False] * len(boxes)
+    """
+    The function itself
+    """
+    if not isinstance(boxes, list):
+        raise TypeError("Input should be a list of lists")
+    n = len(boxes)
+    if n == 0:
+        return False
+    visited = [False] * n
     visited[0] = True
-
     keys = deque(boxes[0])
     while keys:
         key = keys.popleft()
-        if 0 <= key < len(boxes) and not visited[key]:
+        if 0 <= key < n and not visited[key]:
             visited[key] = True
             for new_key in boxes[key]:
-                if not visited[new_key]:
+                if isinstance(new_key, int) and 0 <= new_key < n and not visited[new_key]:
                     keys.append(new_key)
 
     return all(visited)
