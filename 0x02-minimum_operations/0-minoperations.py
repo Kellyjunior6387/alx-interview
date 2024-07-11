df58eb37b48241f0468da5fb3f4cd9a78ce4a9f6
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-12 00:22:05 +0300

    to be continued

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to look for minimal operations"""
+
+def minOperations(n):
+    """
+    The function that return minimum operations
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
