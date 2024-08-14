4d49b501c0ea9b64287123efd9c5a51a9a4d2549
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-14 15:29:48 +0300

    rotating 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module to implement a function that rotate a matrix"""
+from typing import List
+
+
+def transpose(matrix):
+    """A helper function to transpose a matrix"""
+    visited = []
+    rows = len(matrix)
+    for i in range(rows):
+        for j in range(rows):
+            if (i, j) not in visited:
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                visited.append((i, j))
+                visited.append((j, i))
+    return matrix
+
+
+def rotate_2d_matrix(matrix: List[any]) -> List[any]:
+    """A function to rotate a matrix"""
+    transposed_matrix = transpose(matrix)
+    for row in transposed_matrix:
+        row.reverse()
+    return transposed_matrix
