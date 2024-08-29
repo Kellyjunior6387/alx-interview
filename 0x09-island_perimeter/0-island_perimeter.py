8fac017d230fcc1654bc998261c5dcd114179b7d
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-29 12:21:30 +0300

    island perimeter problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,16 +2,6 @@
 """Module to solve the perimeter of an island"""
 
 
-def search_neighbours(i, j, grid) -> int:
-    """Helper function to find if neighbour is land"""
-    if i < 0 or i > len(grid) or j < 0 or j >= len(grid[0]):
-        return 0
-    if grid[i][j] == 1:
-        return 1
-    else:
-        return 0
-
-
 def island_perimeter(grid):
     """
     Function to calculate the perimeter of an island in a grid
@@ -21,8 +11,8 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
-                perimeter -= search_neighbours(i, j - 1, grid)
-                perimeter -= search_neighbours(i - 1, j, grid)
-                perimeter -= search_neighbours(i, j + 1, grid)
-                perimeter -= search_neighbours(i + 1, j, grid)
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
     return perimeter
